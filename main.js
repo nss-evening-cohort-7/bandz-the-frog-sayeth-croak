fd33ce7502c38f78be6c645f723c565960c816fb
Anna Dolan <anna.dolan115@gmail.com>
2018-03-14 18:57:44 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-
+var tourArray = [
+    {
+        date: '08/01/18',
+        time: '9:00pm',
+        city: 'Nashville',
+        venue: "Santa's Pub",
+        tickets: '<a href="#">Tickets</a>'
+    },
+    {
+        date: '08/16/18',
+        time: '8:30pm',
+        city: 'Knoxville',
+        venue: "Back Door Tavern",
+        tickets: ''
+    },
+    {
+        date: '08/24/18',
+        time: '9:15pm',
+        city: 'Bristol',
+        venue: "O'Mainnin's Pub",
+        tickets: ''
+    },
+    {
+        date: '09/09/18',
+        time: '8:00pm',
+        city: 'Memphis',
+        venue: "Lamplighter Lounge",
+        tickets: ''
+    },
+    {
+        date: '09/19/18',
+        time: '9:00pm',
+        city: 'Nashville',
+        venue: "Tin Dog Tavern",
+        tickets: ''
+    }
+]
