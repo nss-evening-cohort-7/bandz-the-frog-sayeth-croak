84dd827a37627ab51caa36b78b3170226bc9b9de
Alex Orga <alexorga.dev@gmail.com>
2018-03-17 09:11:32 -0500

    WIP

--- main.js
+++ main.js
@@ -3,27 +3,27 @@
 
 var bandMembers = [
     {
-        name: 'Poison Dart',
-        instrument: 'Lead Vocals',
-        bio: 'Blah blah blah',
+        name: 'Red Moon',
+        instrument: 'Vocals',
+        bio: "It was what most would call a perfect day.  The sun emblazened it's magnificence on to the landscape below, animals enchantingly sang their songs of joy, a picturesque picnic lunch was strewn across a checkered blanket, and two lovers intertwined below a willow tree.  As the earth and all her creatures were distracted and drenched in euphoria their paradise was being overthrown. A silence grasped the lovers with paranormal force, their lips turned cold as they realized what they had done.  Their act had consumated a creature so vicious and so powerful that when she was born her perfectly pitched cries melted faces and eclipsed the moon with blood.",
         image:
     },
     {
-        name: '',
-        instrument: '',
-        bio: '',
+        name: 'Gastrotheca',
+        instrument: 'Guitar',
+        bio: 'Gastrotheca has been feeding you tasty licks since before Elvis could pick up a rattle.  Yes, he may be the oldest guitar player in the world but when he is done being helped on stage and he sets that tennis ball-bottomed walker aside, you better have your earplugs in because you are about to be headbutted with the loudest rig in history.  We have not determined whether or not this is because he refuses to wear his hearing aids but you, and him for that matter, should consult a physician before he plugs in.',
         image:
     },
     {
-        name: '',
-        instrument: '',
-        bio: '',
+        name: 'Goliath',
+        instrument: 'Bass',
+        bio: "Big A** Dude. Big A** Bass. 'Nuff said.",
         image:
     },
     {
-        name: '',
-        instrument: '',
-        bio: '',
+        name: 'Shovel Nose',
+        instrument: 'Drums',
+        bio: "With quadruple PhD's from MIT Shovel Nose turned his back on academia to pursue a career in the darkest of black metal ",
         image:
     },
     {
