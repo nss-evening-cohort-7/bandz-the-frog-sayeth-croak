7228166eade5b5453832824e799038bdd932d504
Alex Orga <alexorga.dev@gmail.com>
2018-03-16 20:02:44 -0500

    created array of objects named bandMembers and keys for key value pairs in main.js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 
+// Band-Members Data-------------------------------------
+
+var bandMembers = [
+    {
+        name: 'Poison Dart',
+        instrument: 'Lead Vocals',
+        bio: 'Blah blah blah',
+        image:
+    },
+    {
+        name: '',
+        instrument: '',
+        bio: '',
+        image:
+    },
+    {
+        name: '',
+        instrument: '',
+        bio: '',
+        image:
+    },
+    {
+        name: '',
+        instrument: '',
+        bio: '',
+        image:
+    },
+    {
+        name: '',
+        instrument: '',
+        bio: '',
+        image:
+    }
+];
