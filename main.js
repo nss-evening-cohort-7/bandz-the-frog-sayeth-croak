f218c951aea12b4a904a6f6c4db0731e4af7462d
Anna Dolan <anna.dolan115@gmail.com>
2018-03-17 09:27:42 -0500

    finished prinToDom function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+// **************************PRINT TO DOM FUNCTION**************************//
 
+function printToDom(input, id){
+    document.getElementById(id).innerHTML += input;
+}
+
+// *************************************************************************//
