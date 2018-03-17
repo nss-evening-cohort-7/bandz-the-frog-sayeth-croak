58fb22a8b6ab19a9f21e7b0eb9420dae1c3fc33f
Anna Dolan <anna.dolan115@gmail.com>
2018-03-17 09:35:22 -0500

    added tourArray data to main.js

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
+// ***************************TOUR DATA***************************
+
 var tourArray = [
     {
         date: '08/01/18',
         time: '9:00pm',
         city: 'Nashville',
         venue: "Santa's Pub",
-        tickets: '<a href="#">Tickets</a>'
+        tickets: 'Tickets'
     },
     {
         date: '08/16/18',
         time: '8:30pm',
         city: 'Knoxville',
         venue: "Back Door Tavern",
-        tickets: ''
+        tickets: 'Tickets'
     },
     {
         date: '08/24/18',
         time: '9:15pm',
         city: 'Bristol',
         venue: "O'Mainnin's Pub",
-        tickets: ''
+        tickets: 'Tickets'
     },
     {
         date: '09/09/18',
         time: '8:00pm',
         city: 'Memphis',
         venue: "Lamplighter Lounge",
-        tickets: ''
+        tickets: 'Tickets'
     },
     {
         date: '09/19/18',
         time: '9:00pm',
         city: 'Nashville',
         venue: "Tin Dog Tavern",
-        tickets: ''
+        tickets: 'Tickets'
     }
 ]
 
+// **************************************************************//
 
 
